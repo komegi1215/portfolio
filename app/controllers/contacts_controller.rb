bdfa79a13d8aad33338db7da2b3c978d16332cfb
Megumi Hirao <komegi.blog@gmail.com>
2024-03-17 21:59:25 +0900

    changed controller for the form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,24 +19,24 @@ class ContactsController < ApplicationController
     end
   end
 
-  def confirm
-    @contact = Contact.new(contact_params)
-    if @contact.valid?
-      render :action => 'confirm'
-    else
-      render :action => 'index'
-    end
-  end
+  # def confirm
+  #   @contact = Contact.new(contact_params)
+  #   if @contact.valid?
+  #     render :action => 'confirm'
+  #   else
+  #     render :action => 'index'
+  #   end
+  # end
 
-  def done
-    @contact = Contact.new(contact_params)
-    if params[:back]
-      render :action => 'index'
-    else
-      ContactMailer.send_mail(@contact).deliver_now
-      render :action => 'done'
-    end
-  end
+  # def done
+  #   @contact = Contact.new(contact_params)
+  #   if params[:back]
+  #     render :action => 'index'
+  #   else
+  #     ContactMailer.send_mail(@contact).deliver_now
+  #     render :action => 'done'
+  #   end
+  # end
 
   private
 
