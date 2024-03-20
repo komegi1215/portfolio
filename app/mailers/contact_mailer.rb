525bc922c7d4317018163b84d952febead12fc83
Megumi Hirao <komegi.blog@gmail.com>
2024-03-20 15:39:50 +0900

    Set up gmail account to receive message

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,13 @@ class ContactMailer < ApplicationMailer
 
   def send_mail(contact)
     @contact = contact
-    mail to: @contact.email, bcc: ENV["ACTION_MAILER_USER"], subject: "Thank you for your inquiry!"
+    mail to: @contact.email, bcc: ENV['MAIL_ADDRESS'], subject: "Thank you for your inquiry!"
   end
+
+  # def contact
+  #   contact = Contact.new(title: "Test", first_name: "太郎", last_name: "侍", email: "test@gmail.com", message: "問い合わせメッセージ")
+
+  #   ContactMailer.send_mail(contact)
+  # end
 
 end
