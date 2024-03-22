bada14a9b5256eb259a4474c3b15d01a9e1c5841
Megumi Hirao <komegi.blog@gmail.com>
2024-03-22 19:37:50 +0900

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,15 +91,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.smtp_settings = {
-  #   :address => "smtp.gmail.com",
-  #   :port => 587,
-  #   :user_name => ENV['MAIL_ADDRESS'],
-  #   :password => ENV['MAIL_PASSWORD'],
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.default_url_options = { host: 'www.mizmeg.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['SMTP_USERNAME'], # Update with your SMTP username
+    password:             ENV['SMTP_PASSWORD'], # Update with your SMTP password
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
