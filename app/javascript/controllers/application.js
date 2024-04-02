90d379482ddd1ba9420ae7c169522509cdd368e6
Megumi Hirao <komegi.blog@gmail.com>
2024-04-03 00:04:55 +0900

    Fixed syntax error for stimulus contoller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,18 @@
+// import { Application } from "@hotwired/stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+// const application = Application.start()
+// const context = require.context("./controllers", true, /\.js$/);
+// application.load(definitionsFromContext(context));
+
+// // Configure Stimulus development experience
+// application.debug = false
+// window.Stimulus   = application
+
+// export { application }
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
 
 // Configure Stimulus development experience
 application.debug = false
