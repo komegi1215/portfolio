9fb6aa343c53213533cbcd3eef7d118604a3f5bc
Megumi Hirao <komegi.blog@gmail.com>
2024-04-05 22:55:39 +0900

    Completed profile text change

--- app/javascript/controllers/profile_controller.js
+++ app/javascript/controllers/profile_controller.js
@@ -10,17 +10,11 @@ export default class extends Controller {
   }
 
   showMore() {
-    const linkUrl = this.data.get("linkUrl");
-    const linkText = this.data.get("linkText");
-
-    this.profileTextTarget.innerHTML = `<p class='description' data-action='mouseout->profile#showLess'>フリーランスのウェブディベロッパー、データエンジニア。<a href="${linkUrl}" class="link">${linkText}</a>の６ヶ月のブートキャンプをフルタイムで仕事をしなが卒業。現在は製薬メーカーでCRMスペシャリストとしてビジネスオペレーションの改善を実施。データマネジメントやデータエンジニアの領域にも挑戦中。</p>`;
+    this.profileTextTarget.innerHTML = `<p class='description' data-action='mouseout->profile#showLess'>フリーランスのウェブディベロッパー。<a href="https://www.lewagon.com/", class = "link", style = "text-decoration:none;">Le Wagon</a>の６ヶ月のウェブ開発のブートキャンプをフルタイムで仕事をしながら卒業。Le WagonでTAをしつつ、フリーランスのディベロッパーとしてスキルを磨く。現在は製薬メーカーでCRMスペシャリストとしてビジネスオペレーションの改善を注力しながら、データマネジメントやデータエンジニアの領域にも挑戦中。</p>`;
   }
 
   showLess() {
-    const linkUrl = this.data.get("linkUrl");
-    const linkText = this.data.get("linkText");
-
-    this.profileTextTarget.innerHTML = `<p class="description" data-action="mouseover->profile#showMore">I am a freelance web developer and data engineer with a strong passion for coding. I successfully completed the rigorous <a href="${linkUrl}" class="link">${linkText}</a>, a six-month program that honed my coding skills while balancing full-time work commitments.<br>
+    this.profileTextTarget.innerHTML = `<p class="description" data-action="mouseover->profile#showMore">I am a freelance web developer and data engineer with a strong passion for coding. I successfully completed the rigorous <a href="https://www.lewagon.com/", class = "link", style = "text-decoration:none;">Le Wagon Bootcamp</a>, a six-month program that honed my coding skills while balancing full-time work commitments.<br>
     Currently, I am employed as a CRM specialist at a pharmaceutical company, where I excel in managing sales operations. Additionally, I have begun delving into the realms of data management and engineering. No code, no life! 💻</p>`;
   }
 }
